7c9a5f2c7eecc3ca48c1982434189dfbf3d3e314
MatsveiDubaleka <futurist.developer@gmail.com>
2025-05-20 13:05:50 +0300

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { Telegraf } from 'telegraf';
-import { z } from 'zod';
 import { connectDB } from './config/database';
-import { User } from './models/User';
+import { BotService } from './services/BotService';
+import { ClaudeService } from './services/ClaudeService';
+import { validateEnv } from './utils/env';
 
 dotenv.config();
 
-const envSchema = z.object({
-  PORT: z.string().transform(Number),
-  BOT_TOKEN: z.string(),
-  MONGO_URI: z.string(),
-  TENDERLAND_API_KEY: z.string(),
-});
-
-export const ENV = envSchema.parse(process.env);
+export const ENV = validateEnv();
 
 const app = express();
 app.use(express.json());
 
-const bot = new Telegraf(ENV.BOT_TOKEN);
+const claudeService = new ClaudeService();
+const botService = new BotService();
 
 connectDB();
 
-bot.use(async (ctx, next) => {
-  if (ctx.from) {
-    await User.findOneAndUpdate(
-      { telegramId: ctx.from.id },
-      {
-        telegramId: ctx.from.id,
-        username: ctx.from.username,
-        firstName: ctx.from.first_name,
-        lastName: ctx.from.last_name,
-      },
-      { upsert: true, new: true },
-    );
-  }
-  return next();
-});
-
-bot.command('start', (ctx) => {
-  ctx.reply('Welcome to the bot! 👋');
-});
-
-bot.command('help', (ctx) => {
-  ctx.reply('Available commands:\n/start - Start the bot\n/help - Show this help message');
-});
-
-bot.catch((err, ctx) => {
-  console.error(`Error for ${ctx.updateType}:`, err);
-  ctx.reply('An error occurred while processing your request.');
-});
-
-bot
-  .launch()
-  .then(() => {
-    console.log('Bot started successfully');
-  })
-  .catch((err) => {
-    console.error('Failed to start bot:', err);
-    process.exit(1);
-  });
+botService.start();
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Bot is running' });
@@ -70,5 +27,5 @@ app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
 });
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGINT', () => botService.stop('SIGINT'));
+process.once('SIGTERM', () => botService.stop('SIGTERM'));
