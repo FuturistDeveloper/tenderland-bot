88c0cb32a62edf777b8f1d644ca2d26986a9b92f
MatsveiDubaleka <futurist.developer@gmail.com>
2025-05-20 13:06:31 +0300

    feat: add response schema

---
+++ src/schemas/tenderland.schema.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+
+export const CreateTaskResponseSchema = z.object({
+    Id: z.number(),
+    Success: z.boolean(),
+    TotalCount: z.number(),
+    CreateDate: z.string().datetime()
+});
+
+export type CreateTaskResponse = z.infer<typeof CreateTaskResponseSchema>;
+
+export const CustomerSchema = z.object({
+    lotCustomerShortName: z.string()
+});
+
+export const TenderSchema = z.object({
+    regNumber: z.string(),
+    name: z.string(),
+    beginPrice: z.number(),
+    publishDate: z.string().datetime(),
+    endDate: z.string().datetime(),
+    region: z.string(),
+    typeName: z.string(),
+    lotCategories: z.array(z.string()),
+    files: z.string().url(),
+    module: z.string(),
+    etpLink: z.string().url(),
+    customers: z.array(CustomerSchema)
+});
+
+export const TenderItemSchema = z.object({
+    ordinalNumber: z.number(),
+    tender: TenderSchema
+});
+
+export const TendersResponseSchema = z.object({
+    items: z.array(TenderItemSchema)
+});
+
+export type TendersResponse = z.infer<typeof TendersResponseSchema>; 
