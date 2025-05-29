c8b8c6721be18b1927e89d70fa5c96ee3d0f3df7
MatsveiDubaleka <me@duboleko.ru>
2025-05-29 14:48:27 +0300

    feat: comment temporalely

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { getConfig } from './config/config';
 import { connectDB } from './config/database';
 import { BotService } from './services/BotService';
-import { TenderAnalyticsService } from './services/TenderAnalyticsService';
+// import { TenderAnalyticsService } from './services/TenderAnalyticsService';
 import { TenderlandService } from './services/TenderlandService';
 import { validateEnv } from './utils/env';
 import { Context } from 'telegraf';
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 
 const tenderlandService = new TenderlandService(config);
-const tenderAnalyticsService = new TenderAnalyticsService(config);
+// const tenderAnalyticsService = new TenderAnalyticsService(config);
 const geminiService = new GeminiService(config);
 const botService = new BotService();
 
@@ -61,38 +61,39 @@ export const getAnalyticsForTenders = async (
     }
 
     // // 2 STEP: Анализ тендера с помощью Gemini Pro
-    const claudeResponse = await tenderAnalyticsService.analyzeTender(
-      tender.regNumber,
-      unpackedFiles.files,
-    );
+    // const claudeResponse = await tenderAnalyticsService.analyzeTender(
+    //   tender.regNumber,
+    //   unpackedFiles.files,
+    // );
 
-    // // 3 STEP: Удалить распакованные файлы
-    await tenderlandService.cleanupExtractedFiles(unpackedFiles.parentFolder);
+    // // // 3 STEP: Удалить распакованные файлы
+    // await tenderlandService.cleanupExtractedFiles(unpackedFiles.parentFolder);
 
-    if (!claudeResponse) {
-      console.error('[getAnalyticsForTenders] Не удалось получить ответ Gemini');
-      return 'Не удалось получить ответ от ИИ';
-    }
+    // if (!claudeResponse) {
+    //   console.error('[getAnalyticsForTenders] Не удалось получить ответ Gemini');
+    //   return 'Не удалось получить ответ от ИИ';
+    // }
 
-    // 4 STEP: Анализ товаров
-    const isAnalyzed = await tenderAnalyticsService.analyzeItems(tender.regNumber, claudeResponse);
+    // // 4 STEP: Анализ товаров
+    // const isAnalyzed = await tenderAnalyticsService.analyzeItems(tender.regNumber, claudeResponse);
 
-    if (isAnalyzed) {
-      // 5 STEP: Генерация отчета
-      const finalReport = await tenderAnalyticsService.generateFinalReport(tender.regNumber);
+    // if (isAnalyzed) {
+    //   // 5 STEP: Генерация отчета
+    //   const finalReport = await tenderAnalyticsService.generateFinalReport(tender.regNumber);
 
-      if (finalReport) {
-        const halfLength = Math.ceil(finalReport.length / 2);
-        await ctx.reply(finalReport.slice(0, halfLength));
-        await ctx.reply(finalReport.slice(halfLength));
-      } else {
-        await ctx.reply('Не удалось получить ответ от ИИ');
-      }
+    //   if (finalReport) {
+    //     const halfLength = Math.ceil(finalReport.length / 2);
+    //     await ctx.reply(finalReport.slice(0, halfLength));
+    //     await ctx.reply(finalReport.slice(halfLength));
+    //   } else {
+    //     await ctx.reply('Не удалось получить ответ от ИИ');
+    //   }
 
-      return 'Анализ тендера завершен';
-    } else {
-      return 'Не удалось проанализировать товары';
-    }
+    //   return 'Анализ тендера завершен';
+    // } else {
+    //   return 'Не удалось проанализировать товары';
+    // }
+    return 'bla';
   } catch (err) {
     console.error('Error in analytics job:', err);
     return `Произошла ошибка при анализе тендера: ${regNumber}`;
