cf20ccd101026b3ffac5daf69831776c7930dc15
MatsveiDubaleka <me@duboleko.ru>
2025-05-29 14:26:38 +0300

    feat: add phantom

--- src/types/html-pdf.d.ts
+++ src/types/html-pdf.d.ts
@@ -1,24 +1,21 @@
 declare module 'html-pdf' {
-    interface PDFOptions {
-        format?: string;
-        border?: {
-            top?: string;
-            right?: string;
-            bottom?: string;
-            left?: string;
-        };
-    }
-
-    interface PDF {
-        toFile(
-            filePath: string,
-            callback: (error: Error | null, result?: any) => void
-        ): void;
-    }
-
-    function create(html: string, options?: PDFOptions): PDF;
-
-    export = {
-        create
+  interface PDFOptions {
+    format?: string;
+    border?: {
+      top?: string;
+      right?: string;
+      bottom?: string;
+      left?: string;
     };
-} 
+  }
+
+  interface PDF {
+    toFile(filePath: string, callback: (error: Error | null, result?: any) => void): void;
+  }
+
+  function create(html: string, options?: PDFOptions): PDF;
+
+  export = {
+    create,
+  };
+}
