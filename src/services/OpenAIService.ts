3eb174d3fe3d85ffe36341aa1ae6914aff6f8b9c
MatsveiDubaleka <me@duboleko.ru>
2025-05-30 10:25:55 +0300

    feat: add open ai

--- src/services/OpenAIService.ts
+++ src/services/OpenAIService.ts
@@ -1,23 +1,41 @@
 import OpenAI from 'openai';
-import { Config } from '../config/config';
 import { ENV } from '../index';
 
 export class OpenAIService {
-  private readonly config: Config;
   private readonly client: OpenAI;
 
-  constructor(config: Config) {
-    this.config = config;
-
+  constructor() {
     this.client = new OpenAI({
       apiKey: ENV.OPENAI_API_KEY,
     });
   }
 
-  public async generateResponse(prompt: string) {
+  public async generateResponse(prompt: string): Promise<string | null> {
+    console.log('[OpenAI generateResponse] Начало операции');
     try {
       const response = await this.client.chat.completions.create({
-        model: 'gpt-4o-mini',
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: prompt }],
+      });
+      console.log('OpenAI response:', response.choices[0].message.content);
+      return response.choices[0].message.content || null;
+    } catch (error) {
+      console.log('[OpenAI generateResponse] Произошла ошибка при выполнении операции');
+      if (error instanceof Error) {
+        console.error('Error details:', {
+          message: error.message,
+          name: error.name,
+          stack: error.stack,
+        });
+      }
+      return null;
+    }
+  }
+
+  public async generateFinalRequest(prompt: string): Promise<string | null> {
+    try {
+      const response = await this.client.chat.completions.create({
+        model: 'o3',
         messages: [
           {
             role: 'system',
@@ -35,6 +53,7 @@ export class OpenAIService {
       console.log('OpenAI response:', response.choices[0].message.content);
       return response.choices[0].message.content || null;
     } catch (error) {
+      console.log('[OpenAI generateFinalRequest] Произошла ошибка при выполнении операции');
       if (error instanceof Error) {
         console.error('Error details:', {
           message: error.message,
@@ -42,7 +61,7 @@ export class OpenAIService {
           stack: error.stack,
         });
       }
-      return '[OpenAIService] Произошла ошибка при выполнении операции';
+      return null;
     }
   }
 }
